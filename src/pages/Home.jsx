fdec67b8d5f1aab31c336ee590c59c9222fb9112
artur.mineiro <arturmineiro2@hotmail.com>
2024-08-19 14:52:21 -0300

    home concluida

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,13 +18,15 @@ function Home() {
         </section>
   
         <section className="mb-4">
-          <h2 className="h4">API 2: Nome da API</h2>
-          <p>Descrição breve do que essa API faz e os dados que você está exibindo.</p>
-          {/* Adicione aqui os dados da segunda API */}
+          <h2 className="h4">API 2: GPT project</h2>
+          <p>Abaixo tentei consumir uma api do chat gpt, porém acredito ser pago e recebo erro de que estrapolei o limite</p>
+          <Link to="/Gpt" className="btn btn-primary">
+          Ver detalhes
+        </Link>
         </section>
   
         <footer className="mt-4 pt-3 border-top text-center">
-          <p>Feito por [Seu Nome].</p>
+          <p>Feito por [Artur Mineiro].</p>
         </footer>
       </div>
     );
