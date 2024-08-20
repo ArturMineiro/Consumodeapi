5d3338a730cd24b123120c9b25875d82cc1941ff
artur.mineiro <arturmineiro2@hotmail.com>
2024-08-20 12:02:40 -0300

    cats apis

--- src/pages/Cat.jsx
+++ src/pages/Cat.jsx
@@ -2,29 +2,43 @@ import React, { useEffect, useState } from 'react';
 
 function Cat() {
   const [catImage, setCatImage] = useState('');
+  const [breedInfo, setBreedInfo] = useState(null);
   const apiKey = import.meta.env.VITE_CAT_API_KEY;
 
-  useEffect(() => {
-    const fetchCatImage = async () => {
-      try {
-        const response = await fetch('https://api.thecatapi.com/v1/images/search', {
-          headers: {
-            'x-api-key': apiKey
-          }
-        });
-        const data = await response.json();
-        setCatImage(data[0]?.url); // Pegue a URL da primeira imagem retornada
-      } catch (error) {
-        console.error('Erro ao buscar a imagem do gato:', error);
-      }
-    };
+  // Função para buscar dados do gato
+  const fetchCatData = async () => {
+    try {
+      const response = await fetch('https://api.thecatapi.com/v1/images/search?has_breeds=1', {
+        headers: {
+          'x-api-key': apiKey
+        }
+      });
+      const data = await response.json();
+      setCatImage(data[0]?.url); // Pega a URL da imagem
+      setBreedInfo(data[0]?.breeds[0]); // Pega as informações da raça
+    } catch (error) {
+      console.error('Erro ao buscar dados do gato:', error);
+    }
+  };
 
-    fetchCatImage();
-  }, [apiKey]);
+  // Chama a função de busca ao carregar o componente
+  useEffect(() => {
+    fetchCatData();
+  }, []);
 
   return (
     <div>
-      {catImage ? <img src={catImage} alt="A cute cat" /> : <p>Carregando...</p>}
+      {catImage ? <img src={catImage} alt="A cute cat" width={'30%'} /> : <p>Carregando...</p>}
+      {breedInfo && (
+        <div>
+          <h2>{breedInfo.name}</h2>
+          <p>Temperamento: {breedInfo.temperament}</p>
+          <p>Origem: {breedInfo.origin}</p>
+          <a href={breedInfo.wikipedia_url} target="_blank" rel="noopener noreferrer">Saiba mais sobre a raça</a>
+        </div>
+      )}
+      {/* Botão para atualizar o gato */}
+      <button onClick={fetchCatData}>Atualizar Gato</button>
     </div>
   );
 }
