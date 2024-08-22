c89cc8108846fa40c82a2e1cded46f6565a4dc2a
artur.mineiro <arturmineiro2@hotmail.com>
2024-08-22 14:59:16 -0300

    FALTA CORRIGIR BUG DO BTC#

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -1,59 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
-
-
 function Coin() {
-    const [rates, setRates] = useState({});
-    const [baseCurrency, setBaseCurrency] = useState('EUR'); // Moeda base padrão
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-  
-    const fetchRates = async () => {
-      setLoading(true);
-      try {
-        const apiKey = import.meta.env.VITE_COIN_API_KEY;
-        const response = await axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=${apiKey}&base=${baseCurrency}`);
-        setRates(response.data.rates);
-      } catch (err) {
-        setError('Erro ao buscar cotações');
-      } finally {
-        setLoading(false);
+  const [rates, setRates] = useState({});
+  const [baseCurrency, setBaseCurrency] = useState('USD'); 
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchRates = async () => {
+    setLoading(true);
+    try {
+      // Dependendo da moeda base selecionada, define os pares de moedas a serem buscados
+      let currencies = '';
+      if (baseCurrency === 'USD') {
+        currencies = 'BRL-USD,BTC-USD';
+      } else if (baseCurrency === 'BRL') {
+        currencies = 'USD-BRL,BTC-BRL';
+      } else if (baseCurrency === 'BTC') {
+        currencies = 'BTC-USD,BTC-BRL'; 
       }
-    };
-  
-    useEffect(() => {
-      fetchRates();
-    }, [baseCurrency]);
-  
-    return (
-      <div className="container">
-        <h3>Cotações de Moedas (A conversão da erro por conta do plano contratado,porém apresenta apenas o valor do EUR em outras moedas)</h3>
-        {loading ? (
-          <p>Carregando...</p>
-        ) : error ? (
-          <p>{error}</p>
-        ) : (
-          <div>
-            <select onChange={(e) => setBaseCurrency(e.target.value)} value={baseCurrency}>
-              {Object.keys(rates).map((currency) => (
-                <option key={currency} value={currency}>
-                  {currency}
-                </option>
-              ))}
-            </select>
-            <ul>
-              {Object.entries(rates).map(([currency, rate]) => (
-                <li key={currency}>
-                  {currency}: {rate}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    );
-  }
-  
-  export default Coin;
+
+      const response = await axios.get(`https://economia.awesomeapi.com.br/json/last/${currencies}`);
+      setRates(response.data);
+    } catch (err) {
+      setError('Erro ao buscar cotações');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRates();
+  }, [baseCurrency]);
+
+  return (
+    <div className="container">
+      <h3>Cotações de Moedas</h3>
+      {loading ? (
+        <p>Carregando...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <div>
+          <select onChange={(e) => setBaseCurrency(e.target.value)} value={baseCurrency}>
+            <option value="USD">USD</option>
+            <option value="BRL">BRL</option>
+            <option value="BTC">BTC</option>
+          </select>
+          <ul>
+            {Object.entries(rates).map(([currencyPair, data]) => (
+              <li key={currencyPair}>
+                {data.code}: {data.bid}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Coin;
